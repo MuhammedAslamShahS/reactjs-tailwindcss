0f3470da3af2e81a2eda03f545e29ab42ad8d594
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-04-05 14:50:40 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="w-full h-16 bg-slate-500 flex justify-between items-center px-8">
+      <h1 className="text-2xl font-semibold">Newwesterlund</h1>
+
+      <ul className="flex gap-6">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
