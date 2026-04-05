d9a054bca38d0a43c73ce4d2ef5e33a9f81b486c
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-04-05 16:45:52 +0530

    updated about page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+    return (
+        <div className="w-[100%] h-[12vh] bg-black text-white flex justify-evenly items-center">
+            <div><h1 className="text-[1.8rem] font-medium">Branding</h1></div>
+            <div><h1 className="text-[1.8rem] font-medium">Website</h1></div>
+            <div><h1 className="text-[1.8rem] font-medium">Digital Marketing</h1></div>
+            <div><h1 className="text-[1.8rem] font-medium">App</h1></div>
+        </div>
+    );
+};
 
-export default Services
+export default Services;
