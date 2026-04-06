b89c86bb06dbb0a3496df1b50b95fefbcca99b2f
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-04-06 10:48:30 +0530

    updated about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Button from "../components/Button";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+    return (
+        <div className="w-[100%] h-[45vh] bg-[#F5F5F5] px-[50px] flex">
+            <div className="w-[50%] h-[100%] flex items-center">
+                <h1 className="text-[2rem] font-semibold">
+                    Lorem, ipsum. <br /> Lorem, ipsum. <br /> Lorem, ipsum.
+                </h1>
+            </div>
 
-export default About
+            <div className="w-[50%] h-[100%] flex flex-col justify-center">
+              <p className="w-[70%] text-gray-500">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo architecto eligendi nulla veritatis quaerat impedit. Ex repellat iste accusantium iure, id omnis blanditiis unde?</p>
+            <div className="mt-2"><Button className="mt-2 p-2 rounded-lg w-[160px] bg-black text-white">Get into Touch Now</Button></div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
